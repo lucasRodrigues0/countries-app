e3c5755cdd0111d80e72d2a22e86bda4cb5e3dcf
lucasRodrigues0 <lucasrodrigues119816@gmail.com>
2022-09-11 19:55:00 -0300

    implementação da página de países

---
+++ src/app/modules/paises/paises.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-paises',
+  templateUrl: './paises.component.html',
+  styleUrls: ['./paises.component.less']
+})
+export class PaisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
