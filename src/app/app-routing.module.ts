0a62748de856f20343453394334454e3ca14bb75
lucasRodrigues0 <lucasrodrigues119816@gmail.com>
2023-09-25 14:39:55 -0300

    ajustes de layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetalhesPaisComponent } from './modules/paises/detalhes-pais/detalhes-pais.component';
 
 const routes: Routes = [
   { path: '', loadChildren: () => import('./modules/paises/paises.module').then(m => m.PaisesModule) }  
--- src/app/modules/paises/paises-routing.module.ts
+++ src/app/modules/paises/paises-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { PaisesComponent } from './paises.component';
 import { DetalhesPaisComponent } from './detalhes-pais/detalhes-pais.component';
 
-const routes: Routes = [{ path: '', component: PaisesComponent },
-{path: 'name/:name', component: DetalhesPaisComponent}];
+const routes: Routes = [
+  { path: '', component: PaisesComponent },
+  {path: 'name/:name', component: DetalhesPaisComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
